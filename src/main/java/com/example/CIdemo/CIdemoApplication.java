166a5e4aab2636b6866865b5b1dad58b23f9c80e
Rezoan Shakil Prince <rezoan.shakil@bjitgroup.com>
2024-08-28 15:09:25 +0600

    Add more lines

--- src/main/java/com/example/CIdemo/CIdemoApplication.java
+++ src/main/java/com/example/CIdemo/CIdemoApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CIdemoApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(CIdemoApplication.class, args);
+    System.out.println("--------------------------------Hello---------------------------------");
 	}
 
 }
