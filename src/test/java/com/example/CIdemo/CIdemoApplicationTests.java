2c94934eadc4e178d2bbcf34fb86d3d9831070fb
Rezoan Shakil Prince <rezoan.shakil@bjitgroup.com>
2024-08-28 15:20:19 +0600

    Log in test file

--- src/test/java/com/example/CIdemo/CIdemoApplicationTests.java
+++ src/test/java/com/example/CIdemo/CIdemoApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.CIdemo;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CIdemoApplicationTests {
+  static Logger logger = LoggerFactory.getLogger(CIdemoApplication.class);
 
 	@Test
 	void contextLoads() {
+    logger.info("This is a test file");
 	}
 
 }
