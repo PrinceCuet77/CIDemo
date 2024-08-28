d58640c78cc349b7bbd3fd8fabb152baee03a032
Rezoan Shakil Prince <rezoan.shakil@bjitgroup.com>
2024-08-28 11:35:49 +0600

    first commit

---
+++ src/main/java/com/example/CIdemo/CIdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.CIdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CIdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CIdemoApplication.class, args);
+	}
+
+}
