a6874ff74a977856095a5ccb25d5e2aee4a05e67
Rezoan Shakil Prince <rezoan.shakil@bjitgroup.com>
2024-08-28 15:13:19 +0600

    Add more lines with loggers

--- src/main/java/com/example/CIdemo/CIdemoApplication.java
+++ src/main/java/com/example/CIdemo/CIdemoApplication.java
@@ -1,15 +1,19 @@
 package com.example.CIdemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CIdemoApplication {
 
-	public static void main(String[] args) {
-		
-		SpringApplication.run(CIdemoApplication.class, args);
-    System.out.println("--------------------------------Hello---------------------------------");
-	}
+  static Logger logger = LoggerFactory.getLogger(CIdemoApplication.class);
+
+    public static void main(String[] args) {
+        logger.info("---------------Starting CIdemo-------------------");
+        SpringApplication.run(CIdemoApplication.class, args);
+        System.out.println("--------------------------------Hello---------------------------------");
+    }
 
 }
